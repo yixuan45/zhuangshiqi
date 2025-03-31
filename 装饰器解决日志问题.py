6068d0d8ea3dc113ac5a9f86177e235c1ab89980
yixuan45 <2664113021@qq.com>
2025-04-01 07:43:12 +0800

    创建了装饰器

---
+++ 装饰器解决日志问题.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# def fun1():
+#     print("使用功能1")
+#     print("日志记录")
+#
+#
+# def fun2():
+#     print("使用功能2")
+#     print("日志记录")
+
+def writeLog():
+    print("日志纪录")
+
+
+def func1():
+    print("使用功能1")
+    writeLog()
+
+
+def fun2():
+    print("使用功能2")
+    writeLog()
